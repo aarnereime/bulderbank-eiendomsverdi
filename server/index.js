1858c6076a2951f48bd032ac1484cd9e4501b7c2
Årne Reime <aarnereime01@gmail.com>
2022-09-26 13:21:27 +0200

    node backend

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello world!" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
