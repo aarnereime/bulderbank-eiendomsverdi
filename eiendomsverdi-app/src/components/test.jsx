bbba2e063a9d8ce49191a9dd90cfe5bd7d41d1d0
Årne Reime <aarnereime01@gmail.com>
2022-10-06 13:49:54 +0200

    Fixed backend with async/await to get api key

---
+++ eiendomsverdi-app/src/components/test.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const test = () => {
+  const [loading, setLoading] = useState(true);
+
+  const [allValues, setAllValues] = useState({
+    firstname: "",
+    lastname: "",
+    gender: "",
+  });
+
+  // useEffect(() => {
+  //   const loadData = async () => {
+  //     const response = await fetch("https://api.randomuser.me/");
+  //     const data = await response.json();
+  //     setAllValues({
+  //       ...allValues,
+  //       firstname: data.results[0].name.first,
+  //       lastname: data.results[0].name.last,
+  //       gender: data.results[0].gender,
+  //     });
+  //     setLoading(false);
+  //   };
+
+  //   loadData();
+  // }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://api.randomuser.me/")
+      .then((response) => {
+        setAllValues({
+          firstname: response.data.results[0].name.first,
+          lastname: response.data.results[0].name.last,
+          gender: response.data.results[0].gender,
+        });
+      })
+      .catch((error) => {
+        console.error(`Error fetching data: ${error}`);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      {loading ? <div>Loading...</div>: <div>Hello {allValues.firstname} {allValues.lastname}, your gender is {allValues.gender}</div>}
+    </div>
+  );
+};
+
+export default test;
--- server/index.js
+++ server/index.js
@@ -1,23 +1,74 @@
 // server/index.js
 
-const express = require("express");
-const axios = require("axios")
-const dotenv = require("dotenv")
+const app = require("express");
+const axios = require("axios");
+const dotenv = require("dotenv");
 
 const PORT = process.env.PORT || 3001;
-const app = express();
+
+
+//webapps-api.test.bulderbank.tech/Edv/getedvtoken
+
+let accessTokenKey = "";
+let apiInfo = "";
+
+const accessTokenURL =
+  "https://webapps-api.test.bulderbank.tech/Edv/getedvtoken";
+
+const axios = require("axios");
+
+const api_key = "";
+
+//Henter ut access token
+const getApiKey = async () => {
+  try {
+    const resp = await axios.get(accessTokenURL, {
+      method: "GET",
+      headers: {
+        authorization: "Basic " + api_key,
+      },
+    });
+    accessTokenKey = resp.data.accessToken;
+  } catch (error) {
+    console.error(error);
+  }
+};
+getApiKey();
+
+
+//Henter ut info fra EDV API
+const getEindomsVerdiAPI = async () => {
+  try {
+    await getApiKey();
+    const resp = await axios.get(
+      "https://api.eiendomsverdi.no/realproperty/v1/RealEstates/3019/132/1065/0/24/attributes",
+      {
+        method: "GET",
+        headers: {
+          authorization: "Bearer " + accessTokenKey,
+        },
+      }
+    );
+    apiInfo = resp.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getEindomsVerdiAPI();
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello world!" });
+  res.json({ apiInfo });
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
+
+
+
 url = "https://webapps-api.test.bulderbank.tech/Edv/getedvtoken"
-
-
 
 dotenv.config();
 const key = process.env.REACT_APP_TOKEN;
