3334f332e4aac79c3dad6101fa7d5090a1d4b119
theOneTrueArtist <snorre.solli.haland@gmail.com>
2022-10-10 10:55:32 +0200

    fixed API variables

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 // server/index.js
 
-const app = require("express");
+const express = require("express");
 const axios = require("axios");
 const dotenv = require("dotenv");
+const app = express();
 
 const PORT = process.env.PORT || 3001;
 
@@ -14,9 +15,8 @@ let apiInfo = "";
 const accessTokenURL =
   "https://webapps-api.test.bulderbank.tech/Edv/getedvtoken";
 
-const axios = require("axios");
-
-const api_key = "";
+dotenv.config();
+const api_key = process.env.REACT_APP_TOKEN;
 
 //Henter ut access token
 const getApiKey = async () => {
@@ -63,39 +63,3 @@ app.get("/api", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-
-
-
-
-url = "https://webapps-api.test.bulderbank.tech/Edv/getedvtoken"
-
-dotenv.config();
-const key = process.env.REACT_APP_TOKEN;
-
-const options = {
-  headers: {
-    
-    'Authorization': `Basic ${key}`
-  }
-};
-
-axios.get(url, options)
-  .then(res=> {
-    console.log(res.data.accessToken)
-    key = res.data.accessToken;
-  })
-  .catch(err=> console.log(err));
-
-/*
-url = "https://api.eiendomsverdi.no/realproperty/v1/RealEstates/3019/132/1065/0/24/attributes"
-
-options = {
-  headers: {
-    'Authorization': `Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1qZzBSakkxUWpBek1UVkNNVFEwUlVVNE1qQkdRVU00TVRJNE5qSkdPRVkwT0RNMU9FVTJNQSJ9.eyJodHRwczovL2VpZW5kb21zdmVyZGkubm8vZXZVc2VySWQiOjU2NjMsImh0dHBzOi8vZWllbmRvbXN2ZXJkaS5uby9ldkNsaWVudE5hbWUiOiJCdWxkZXIgQmFuayIsImh0dHBzOi8vZWllbmRvbXN2ZXJkaS5uby9ldlBlcm1pc3Npb25zIjpbInJlYWxwcm9wZXJ0eTpvd25lciIsInJlYWxwcm9wZXJ0eTpyZWFsZXN0YXRlIiwicmVhbHByb3BlcnR5OmhvdXNpbmdjb29wZXJhdGl2ZSIsInJlYWxwcm9wZXJ0eTpzcGVjaWFsY29uc3RyYWludHMiLCJyZWFscHJvcGVydHk6bW9ydGdhZ2UiLCJyZWFscHJvcGVydHk6ZW52aXJvbm1lbnRhbCIsInJlYWxwcm9wZXJ0eTpsYW5kcmVnaXN0cnkiXSwiaXNzIjoiaHR0cHM6Ly9sb2dpbi5laWVuZG9tc3ZlcmRpLm5vLyIsInN1YiI6Ilp4U3BoREhGWm9vbzRTWllFOHBFek94RkxJYVBhS1NnQGNsaWVudHMiLCJhdWQiOiJodHRwczovL2FwaS5laWVuZG9tc3ZlcmRpLm5vIiwiaWF0IjoxNjY1MDQ0ODk4LCJleHAiOjE2NjUxMzEyOTgsImF6cCI6Ilp4U3BoREhGWm9vbzRTWllFOHBFek94RkxJYVBhS1NnIiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIiwicGVybWlzc2lvbnMiOltdfQ.IdaZF6xRAFWdm5-5TwrkIzcp-h3oUeE41Fq6OyMsD_hOHqq2DsV0EJcwq92Cw-8eurmkmM4LhfdJG2SdY7Ne5o1W4VO0EhIV5KfF---Dz9GFj78yA9fr1yFKsv62cEvKmVgG1SHQb9Z70OzRcgQBskE2-nBpQ7t42lXxSR4keoV_V0B0SVKQBh6CYniPgYqBAay2lVmH90lfVYlwTjPGdzXVZC12ZVPcVeGRQMH0d1pBcuqnvcRTbxc3hpEICqS1PNVzeK7pJmsDueNZ3dvER_W83SDPEb6LZUsQ8OUIFOkdewlMnGy-Tmzcj6TFF-atsN67WuIGwq-pSJR-uDxYjA`
-  }
-};
-
-axios.get(url, options)
-  .then(res=> console.log(res.data))
-  .catch(err=> console.log(err))
-*/
